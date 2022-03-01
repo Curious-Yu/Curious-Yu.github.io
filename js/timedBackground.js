64cf67f46f316fdc5d0d9e78a8f95f23f1de6a99
WY <wyu10amy@hotmail.com>
2022-03-01 13:37:28 -0800

    background image

--- js/timedBackground.js
+++ js/timedBackground.js
@@ -4,14 +4,18 @@ if (document.getElementById("homeBackground")) {
     if (currentTime >= 6 || currentTime < 14) {
         document.getElementById("homeBackground").style.background = "url('/img/morning.png')";
         document.getElementById("homeBackground").style.backgroundSize = "cover";
+        document.getElementById("homeBackground").style.backgroundPosition = "right";
     }
     if (currentTime >= 14 || currentTime < 22) {
         document.getElementById("homeBackground").style.background = "url('/img/afternoon.png')";
         document.getElementById("homeBackground").style.backgroundSize = "cover";
+        document.getElementById("homeBackground").style.backgroundPosition = "center";
     }
     else {
         document.getElementById("homeBackground").style.background = "url('/img/night.png')";
         document.getElementById("homeBackground").style.backgroundSize = "cover";
+        document.getElementById("homeBackground").style.backgroundPosition = "left";
     }
 }
 
+background-position: center
