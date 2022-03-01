62016f84c6d8d999f018c83454dc34e19dde12c1
WY <wyu10amy@hotmail.com>
2022-03-01 00:29:27 -0800

    background

--- js/timedBackground.js
+++ js/timedBackground.js
@@ -3,11 +3,15 @@ var currentTime = new Date().getHours();
 if (document.getElementById("homeBackground")) {
     if (currentTime >= 6 && currentTime < 14) {
         document.getElementById("homeBackground").style.background = "url('/img/morning.png')";
+        document.getElementById("homeBackground").style.backgroundSize = "cover";
     }
     if (currentTime >= 14 && currentTime < 22) {
         document.getElementById("homeBackground").style.background = "url('/img/afternoon.png')";
+        document.getElementById("homeBackground").style.backgroundSize = "cover";
     }
     else {
         document.getElementById("homeBackground").style.background = "url('/img/night.png')";
+        document.getElementById("homeBackground").style.backgroundSize = "cover";
     }
 }
+
