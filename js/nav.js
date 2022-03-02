88ccedb5da593536e2c395417117047f82788658
WY <wyu10amy@hotmail.com>
2022-03-02 00:22:16 -0800

    styling

--- js/nav.js
+++ js/nav.js
@@ -3,9 +3,9 @@ function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "flex") {
       x.style.display = "none";
-      document.getElementById("icon").innerHTML = '<i class="fas fa-server"></i>';
+      document.getElementById("icon").innerHTML = '<i class="fas fa-server" title="menu"></i>';
     } else {
       x.style.display = "flex";
-      document.getElementById("icon").innerHTML = '<i class="fa fa-times-circle-o"></i>';
+      document.getElementById("icon").innerHTML = '<i class="fa fa-times-circle-o" title="close menu"></i>';
     }
   }
