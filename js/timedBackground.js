0257a537f01c07db9cedb0a08d74ded61a8ec6b8
WY <wyu10amy@hotmail.com>
2022-03-01 22:51:33 -0800

    background and menu bar

--- js/timedBackground.js
+++ js/timedBackground.js
@@ -1,16 +1,19 @@
 var currentTime = new Date().getHours();
 
+console.log(currentTime);
+
 if (document.getElementById("homeBackground")) {
-    if (currentTime >= 6 || currentTime < 14) {
+    if (currentTime > 6 && currentTime <= 13) {
         document.getElementById("homeBackground").style.background = "url('/img/morning.png')";
         document.getElementById("homeBackground").style.backgroundSize = "cover";
         document.getElementById("homeBackground").style.backgroundPosition = "right";
     }
-    if (currentTime >= 14 || currentTime < 22) {
+    if (currentTime > 13 && currentTime <= 20) {
         document.getElementById("homeBackground").style.background = "url('/img/afternoon.png')";
         document.getElementById("homeBackground").style.backgroundSize = "cover";
         document.getElementById("homeBackground").style.backgroundPosition = "center";
     }
+
     else {
         document.getElementById("homeBackground").style.background = "url('/img/night.png')";
         document.getElementById("homeBackground").style.backgroundSize = "cover";
