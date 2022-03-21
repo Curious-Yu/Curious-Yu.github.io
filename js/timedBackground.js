293cab8343a123d67c413c7d795dc746d0d8cc85
WY <wyu10amy@hotmail.com>
2022-03-21 01:16:40 -0700

    styling

--- js/timedBackground.js
+++ js/timedBackground.js
@@ -6,26 +6,22 @@ if (document.getElementById("homeBackground")) {
 
 
     if (currentTime > 20) {
-        document.getElementById("homeBackground").style.background = "url('/img/night.png')";
+        document.getElementById("homeBackground").style.backgroundImage = "url('/img/night.png')";
         document.getElementById("homeBackground").style.backgroundPosition = "left";
-        document.getElementById("homeBackground").style.backgroundSize = "cover";
     }
 
     else if (currentTime > 13) {
-        document.getElementById("homeBackground").style.background = "url('/img/afternoon.png')";
+        document.getElementById("homeBackground").style.backgroundImage = "url('/img/afternoon.png')";
         document.getElementById("homeBackground").style.backgroundPosition = "center";
-        document.getElementById("homeBackground").style.backgroundSize = "cover";
     }
 
     else if (currentTime > 6) {
-        document.getElementById("homeBackground").style.background = "url('/img/morning.png')";
+        document.getElementById("homeBackground").style.backgroundImage = "url('/img/morning.png')";
         document.getElementById("homeBackground").style.backgroundPosition = "right";
-        document.getElementById("homeBackground").style.backgroundSize = "cover";
     }
 
     else {
-        document.getElementById("homeBackground").style.background = "url('/img/night.png')";
+        document.getElementById("homeBackground").style.backgroundImage = "url('/img/night.png')";
         document.getElementById("homeBackground").style.backgroundPosition = "left";
-        document.getElementById("homeBackground").style.backgroundSize = "cover";
     }
 };
